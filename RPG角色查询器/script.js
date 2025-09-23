7f5c7be9e1f73756fab8c2240d54f6486dc40fa3
blankcx330 <2486119856@qq.com>
2025-09-23 22:49:00 +0800

    完成项目

--- RPG角色查询器/script.js
+++ RPG角色查询器/script.js
@@ -4,7 +4,7 @@ const nameText = document.getElementById("creature-name");
 const idText = document.getElementById("creature-id");
 const weightText = document.getElementById("weight");
 const heightText = document.getElementById("height");
-const types = document.getElementById("types-container");
+const typesHtml = document.getElementById("types");
 const hpText = document.getElementById("hp");
 const attackText = document.getElementById("attack");
 const defenseText = document.getElementById("defense");
@@ -13,14 +13,55 @@ const specialDefenseText = document.getElementById("special-defense");
 const speedText = document.getElementById("speed");
 const hide = document.getElementById("hide");
 
+const npcDataUrl = "https://rpg-creature-api.freecodecamp.rocks/api/creature/";
+
 hide.style.visibility = "hidden";
 
 //const data = fetch("https://rpg-creature-api.freecodecamp.rocks/api/creature/").then(response => response.json()).catch(error => console.error("请求失败: ", error));
 
-const fetchData = async () => {
+
+const fetchData = async (url) => {
     try {
-        const data = fetch("https://rpg-creature-api.freecodecamp.rocks/api/creature/")
+        const data = await fetch(url);
+        const dataJson = await data.json();
+        return dataJson;
     } catch (error) {
-        console.log(error)
+        alert( "Creature not found");
     }
 }
+
+const updateText = data => {
+    const {id, name, weight, height} = data;
+    idText.textContent = "#" + id;
+    nameText.textContent = name;
+    weightText.textContent = weight;
+    heightText.textContent = height;
+    hide.style.visibility = "visible"
+}
+
+const updateTypes = data => {
+    typesHtml.innerHTML = "";
+    const {types} = data;
+    for(let i = 0; i < types.length; i++){
+        typesHtml.innerHTML += `<p>${types[i].name}</p>`;
+    }
+}
+
+const updateNpcData = data => {
+    const {stats} = data;
+    hpText.textContent = stats[0].base_stat;
+    attackText.textContent = stats[1].base_stat;
+    defenseText.textContent = stats[2].base_stat;
+    specialAttackText.textContent = stats[3].base_stat;
+    specialDefenseText.textContent = stats[4].base_stat;
+    speedText.textContent = stats[5].base_stat;
+}
+
+searchBtn.addEventListener("click", async () => {
+    const searchValue = searchTnput.value;
+    const url = npcDataUrl + searchValue;
+    const npcData = await fetchData(url);
+    updateText(npcData);
+    updateTypes(npcData);
+    updateNpcData(npcData);
+})
